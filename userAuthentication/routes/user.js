7eee67af6dcd7211793017c641b352214277e271
Justin Gabriel <justin.g@neoito.com>
2023-03-20 11:04:25 +0530

    feature: User password criteria added

--- userAuthentication/routes/user.js
+++ userAuthentication/routes/user.js
@@ -5,8 +5,9 @@ const userSchema = require('../validations/userSchema')
 const emailExistValidation = require('../middlewares/emailExistValidationMiddleware')
 const userNameExistValidation = require('../middlewares/userNameExistValidationMiddleware')
 const userValidation = require('../middlewares/userValidationMiddleware')
+const passwordCriteria = require('../middlewares/passwordCriteriaMiddleware')
 
-router.post("/signup",userNameExistValidation(),userValidation(userSchema.userNameSchema),emailExistValidation(),userValidation(userSchema.emailSchema),userValidation(userSchema.passwordSchema) , userController.signup );
+router.post("/signup",userNameExistValidation(),userValidation(userSchema.userNameSchema),emailExistValidation(),userValidation(userSchema.emailSchema),userValidation(userSchema.passwordSchema) , passwordCriteria(), userController.signup );
 router.post('/signin',userValidation(userSchema.emailSchema), userController.signin)
 
 
